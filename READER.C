40b961c5091f05e2892eff711bf43bb21ad8d6f3
Affonso Amendola <affonso@SerokMinmas.Home>
2018-04-17 22:19:43 -0300

    Put bytetohex code on an individual funcion, and fixed a bunch of things, user interface stuff is better now, untested on MAME yet

--- READER.C
+++ READER.C
@@ -1,69 +1,106 @@
 #include <conio.h>
 #include <dos.h>
 #include <bios.h>
+//Comment these three so that the code will compile on modern OS's, these should compile with Borland C++, Turbo C and the like, 
+//try with some 90s C compiler, it should work
 
-void main()
+//#include <stdio.h>
+
+char * convertByteToHexCode(char c_input);
+
+int main()
 {
-	char buffer[512];
+	char c_buffer[512];
 
-	int i;
-	int disk;
-	int head;
-	int sector;
-	int track;
-	int nsec;
+	int i_disk;
+	int i_head;
+	int i_sector;
+	int i_track;
+	int i_nsec;
+
+	int i_errorCode;
 	
 	printf("Enter number of the disk to read:\n");
-	scanf("%i", &disk);
+	scanf("%i", &i_disk);
 	printf("Enter number of the head to read:\n");
-	scanf("%i", &head);
+	scanf("%i", &i_head);
 	printf("Enter number of the track to read:\n");
-	scanf("%i", &track);
+	scanf("%i", &i_track);
 	printf("Enter number of the sector to read:\n");
-	scanf("%i", &sector);
+	scanf("%i", &i_sector);
+
+	int i;
 
 	for(i=0;i<512;i++)
 	{
 
-		buffer[i] = 'Z';
+		c_buffer[i] = 10;
 
 	}
 
-	printf("\nREADING DISK 2, HEAD 1, TRACK 1, SECTOR 1...\n\n");
+	printf("\nREADING DISK %i",i_disk);
+	printf(", HEAD %i",i_head);
+	printf(", TRACK %i",i_track);
+	printf(", SECTOR %i...\n\n",i_sector);
 
-	printf("%i\n", biosdisk(2, disk, head, track, sector, 1, buffer));
+	i_errorCode = biosdisk(2, i_disk, i_head, i_track, i_sector, 1, c_buffer));
+	//i_errorCode = 0;
 
-	for(i=0;i<512; i++)
+	if(i_errorCode == 0)
 	{
-		
-		char currentData[2];
-		
+		int j = 0;
 
-		//Converting Raw data into readable hex codes, first time doing this, and I have literally no idea if it will work,
-
-		currentData[0]= buffer[i] & 240
-		//Masking with 11110000, returning XXXX0000
-		currentData[0]= currentData[0] >> 4
-		//Shifting bits to the right, returning 0000XXXX
-		currentData[0]= currentData + 48
-		//Adding 48 to place the characters on the right point of the ascii table
-		if(currentData[0] > 57)
+		for(i=0;i<512; i++,j++)
 		{
-			currentData[0]= currentData + 8
-			//Add 8 to skip the punctuations on the ascii table and get to A B C, etc
-		}
-		
-		currentData[1]= buffer[i] & 15
-		//Masking with 00001111 returning only 0000XXXX
-		currentData[1]= currentData + 48
-		//Adding 48 to place the characters on the right point of the ascii table
-		if(currentData[1] > 57)
-		{
-			currentData[1]= currentData + 8
-			//Add 8 to skip the punctuations on the ascii table and get to A B C, etc
-		}
 
-		printf("%c", currentData[0]);
-		printf("%c", currentData[1]);
+			if(j > 15)
+			{
+				//Making pretty columns
+				printf("\n");
+				j=0;
+
+			}	
+
+			char c_hexcode[2];
+			c_hexcode[0] = *convertByteToHexCode(c_buffer[i]);
+			c_hexcode[1] = *convertByteToHexCode(c_buffer[i]);
+
+			printf("%c", c_hexcode[0]);
+			printf("%c ", c_hexcode[1]);
+		}
 	}
-}
+	else
+	{
+		printf("BIOSDISK() returned an error:\nERROR: %i", i_errorCode);
+	}
+}
+
+char * convertByteToHexCode(char c_input)
+{
+	static char c_currentData[2];
+	//Converting Raw data into readable hex codes
+	
+	c_currentData[0]= c_input & 240;
+	//Masking with 11110000, returning XXXX0000
+	c_currentData[0]= c_currentData[0] >> 4;
+	//Shifting bits to the right, returning 0000XXXX
+	c_currentData[0]= c_currentData[0] + 48;
+	//Adding 48 to place the characters on the right point of the ascii table
+	if(c_currentData[0] > 57)
+	{
+		c_currentData[0]= c_currentData[0] + 8;
+		//Add 8 to skip the punctuations on the ascii table and get to A B C, etc
+	}
+	
+	c_currentData[1]= c_input & 15;
+	//Masking with 00001111 returning only 0000XXXX
+	c_currentData[1]= c_currentData[1] + 48;
+	//Adding 48 to place the characters on the right point of the ascii table
+	if(c_currentData[1] > 57)
+	{
+		c_currentData[1]= c_currentData[1] + 7;
+		//Add 7 to skip the punctuations on the ascii table and get to A B C, etc
+	}
+
+	return c_currentData;
+}
